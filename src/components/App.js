8366759a952d83bbacba23f4b37d12dda23ae4bd
Josh Burd <Jburdt@gmail.com>
2022-09-28 00:00:27 -0400

    done

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React from "react";
 import Header from "./Header";
+// import PlantCard from "./PlantCard";
 import PlantPage from "./PlantPage";
 
 function App() {
+
   return (
     <div className="app">
       <Header />
@@ -12,3 +14,10 @@ function App() {
 }
 
 export default App;
+
+
+
+
+// const filteredTerms = () => {
+//   return listings.filter((listObj) => ( listObj.description.toLowerCase().includes(searchTerm.toLowerCase()) || searchTerm === "")) 
+// }
--- src/components/PlantPage.js
+++ src/components/PlantPage.js
@@ -1,14 +1,28 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
 import NewPlantForm from "./NewPlantForm";
 import PlantList from "./PlantList";
 import Search from "./Search";
 
 function PlantPage() {
+  const [plants, setPlants] = useState([])
+  const [searchTerm, setSearchTerm] = useState('')
+  
+
+  const displayPlants = plants.filter((plant) => {
+    return plant.name.toLowerCase().includes(searchTerm.toLowerCase())
+  })
+
+  useEffect(() => {
+    fetch('http://localhost:6001/plants')
+    .then(res => res.json())
+    .then(data => setPlants(data))
+  }, [])  
+
   return (
     <main>
-      <NewPlantForm />
-      <Search />
-      <PlantList />
+      <NewPlantForm plants={plants} />
+      <Search setSearchTerm ={setSearchTerm} searchTerm= {searchTerm}  />
+      <PlantList  plants={displayPlants} setPlants={setPlants}/>
     </main>
   );
 }
